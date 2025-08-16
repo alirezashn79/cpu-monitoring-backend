5c6023ee447ef6c20d65e56c99d947a5ce49f113
alirezashn79 <alireza.sharifinasabb@yahoo.com>
2025-08-16 21:52:16 +0330

    feat: memory

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express from 'express'
 import 'dotenv/config'
-import 'dotenv/config'
 import http from 'node:http'
 import cors from 'cors'
 import { FRONT_END_URL } from './constants/constants'
+import socket from './socket/socket'
 const PORT = process.env.PORT
 
 const app = express()
 
 const server = http.createServer(app)
+
+socket(server)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -20,5 +22,5 @@ app.use(
 )
 
 server.listen(PORT, () => {
-  console.log(`server running on port ${FRONT_END_URL}`)
+  console.log(`server running on port ${PORT}`)
 })
---
+++ src/socket/socket.ts
@@ -0,0 +1,73 @@
+import http from 'node:http'
+import { Server, Socket } from 'socket.io'
+import { FRONT_END_URL } from '../constants/constants'
+import osUtils from 'os-utils'
+import os from 'os'
+
+type TimePoint = [number, number]
+
+export default function socket(server: http.Server): void {
+  const io = new Server(server, {
+    cors: { origin: FRONT_END_URL },
+    transports: ['websocket'],
+  })
+
+  const MAX_HISTORY = 10
+
+  let cpuHistory: TimePoint[] = Array.from({ length: MAX_HISTORY }, (_, i) => [
+    Date.now() - (MAX_HISTORY - i) * 1000,
+    0,
+  ])
+  let memHistory: TimePoint[] = Array.from({ length: MAX_HISTORY }, (_, i) => [
+    Date.now() - (MAX_HISTORY - i) * 1000,
+    0,
+  ])
+
+  let intervalId: NodeJS.Timeout | null = null
+
+  const startPolling = () => {
+    if (intervalId) return
+
+    intervalId = setInterval(() => {
+      const now = Date.now()
+
+      osUtils.cpuUsage((cpuFraction) => {
+        const cpuPercent = Math.round(cpuFraction * 100)
+        cpuHistory = [...cpuHistory.slice(1), [now, cpuPercent]]
+
+        const totalMem = os.totalmem()
+        const freeMem = os.freemem()
+        const usedMemPercent = Math.round(((totalMem - freeMem) / totalMem) * 100)
+        memHistory = [...memHistory.slice(1), [now, usedMemPercent]]
+
+        io.emit('cpu', cpuHistory)
+        io.emit('memory', memHistory)
+      })
+    }, 1000)
+  }
+
+  const stopPolling = () => {
+    if (intervalId) {
+      clearInterval(intervalId)
+      intervalId = null
+    }
+  }
+
+  io.on('connection', (socket: Socket) => {
+    console.log(`Client connected: ${socket.id}`)
+
+    socket.emit('cpu', cpuHistory)
+    socket.emit('memory', memHistory)
+
+    if (io.engine.clientsCount === 1) {
+      startPolling()
+    }
+
+    socket.on('disconnect', () => {
+      console.log(`Client disconnected: ${socket.id}`)
+      if (io.engine.clientsCount === 0) {
+        stopPolling()
+      }
+    })
+  })
+}
